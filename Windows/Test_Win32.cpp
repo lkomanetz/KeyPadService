b1a5f4f0f62fd96af19f3d162a007be00f7eb411
Logan Komanetz <Logan.Komanetz@networkedenergy.com>
2017-05-04 09:32:33 -0500

    R&D for Windows keyboard input.

--- Windows/Test_Win32.cpp
+++ Windows/Test_Win32.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 #include <windows.h>
+#include <winuser.h>
 #include <keypad.h>
 #include <windowsprogram.h>
 
 using namespace std;
 
+void testKbInput();
+
 int main(int argc, char** argv) {
+	/*
 	WindowsProgram* program = new WindowsProgram();
 	WindowsJoystick* js = program->getJoystick();
 
@@ -16,5 +20,40 @@ int main(int argc, char** argv) {
 	}
 
 	delete program;
+	*/
+
+	testKbInput();
 	return 0;
+}
+
+/*
+ * The whole point of this is to see how sending keyboard input works in Windows.  Should give me an idea
+ * on how to extract this into a class.
+ */
+void testKbInput() {
+	Sleep(5000);
+	KeyboardButton right = 0x41;
+
+	KEYBDINPUT kb = {};
+	INPUT input = {};
+
+	kb.wVk = right;
+	kb.dwFlags = 0;
+	input.type = INPUT_KEYBOARD;
+	input.ki = kb;
+
+	// What I'm doing here is seeing how sending two key-down inputs work.
+	// This sends to "a" values as expected.
+	SendInput(1, &input, sizeof(input));
+	SendInput(1, &input, sizeof(input));
+
+	ZeroMemory(&kb, sizeof(KEYBDINPUT));
+	ZeroMemory(&input, sizeof(INPUT));
+
+	kb.dwFlags = KEYEVENTF_KEYUP;
+	kb.wVk = right;
+	input.type = INPUT_KEYBOARD;
+	input.ki = kb;
+
+	SendInput(1, &input, sizeof(input));
 }
