e1ed9dcaf9f383bda16b7dea76e0157d61867987
lkomanetz <logan.komanetz@gmail.com>
2017-05-05 20:57:51 -0500

    Working on Linux keyboard input.

--- Linux/linux_program.cpp
+++ Linux/linux_program.cpp
@@ -2,10 +2,7 @@
 #include <iostream>
 
 LinuxProgram::LinuxProgram() {
-	int fd = open("/dev/input/event4", O_RDWR | O_NONBLOCK);
-	if (fd == -1)
-		std::cout << "Unable to open keyboard" << std::endl;
-
+	// this->getCurrentFocusedWindow();
 	p_joystick = new LinuxJoystick();
 	p_joystick->initialize();
 	pthread_create(&_jsStateThread, NULL, &LinuxProgram::getJoystickState, this);
@@ -86,7 +83,54 @@ void* LinuxProgram::getJoystickState(void* obj) {
 }
 
 void LinuxProgram::sendKeyPress(KeyboardButton button) {
+	int n = 0;
+	int fd = open("/dev/input/event4", O_RDWR | O_NONBLOCK);
+	if (fd == -1) {
+		std::cout << "Unable to open keyboard socket" << std::endl;
+		return;
+	}
+	input_event evt;
+	evt.type = EV_KEY;
+	evt.value = 1;
+	evt.code = KEY_A;
+	gettimeofday(&evt.time, 0);
+	n = write(fd, &evt, sizeof(input_event));
+
+	evt.value = 0;
+	evt.code = KEY_A;
+	gettimeofday(&evt.time, 0);
+	n = write(fd, &evt, sizeof(input_event));
+	close(fd);
+	std::cout << "done" << std::endl;
+	/*
+	XKeyEvent evt = {};
+	evt.display = p_display;
+	evt.window = _focusedWindow;
+	evt.root = _rootWindow;
+	evt.subwindow = None;
+	evt.time = CurrentTime;
+	evt.x = 1;
+	evt.y = 1;
+	evt.x_root = 1;
+	evt.y_root = 1;
+	evt.same_screen = True;
+	evt.keycode = XKeysymToKeycode(p_display, XK_a);
+	evt.state = 0;
+	evt.type = KeyPress;
+
+	XSendEvent(evt.display, evt.window, True, KeyPressMask, (XEvent*)&evt);
+	evt.type = KeyRelease;
+	XSendEvent(evt.display, evt.window, True, KeyPressMask, (XEvent*)&evt);
+	*/
 }
 
 void LinuxProgram::getCurrentFocusedWindow() {
+	p_display = XOpenDisplay(0);
+	if (p_display == NULL) {
+		std::cout << "NO DISPLAY" << std::endl;
+		return;
+	}
+	_rootWindow = XDefaultRootWindow(p_display);
+	int revert;
+	XGetInputFocus(p_display, &_focusedWindow, &revert);
 }
