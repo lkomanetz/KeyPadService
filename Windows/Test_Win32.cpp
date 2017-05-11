30b78850d1e20f434e3e101925ca81903b4f8d4d
Logan Komanetz <Logan.Komanetz@networkedenergy.com>
2017-05-11 09:05:00 -0500

    Refactoring and trying to get CPU usage down.

--- Windows/Test_Win32.cpp
+++ Windows/Test_Win32.cpp
@@ -4,9 +4,11 @@
 #include <keypad.h>
 #include <windowskeyboard.h>
 #include <windowsprogram.h>
+#include <csignal>
 
 using namespace std;
 
+//TODO(Logan) -> Use signals to end the application gracefully
 int main(int argc, char** argv) {
 	if (argc != 2) {
 		std::cerr << "Usage: " << argv[0] << " <FILE_LOCATION>" << std::endl;
@@ -22,6 +24,7 @@ int main(int argc, char** argv) {
 			cerr << "No controller connected..." << endl;
 			break;
 		}
+		Sleep(25);
 	}
 
 	delete program;
