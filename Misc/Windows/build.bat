76c8a9e1497dc0fc7e7020449f6274418b8c09ca
Logan <lkomanetz@gmail.com>
2017-07-21 16:32:54 -0500

    Changing process name

--- Misc/Windows/build.bat
+++ Misc/Windows/build.bat
@@ -6,6 +6,6 @@ pushd Build
 del /q *
 for /d %%x in (*) do @rd /s /q "%%x"
 
-cl -D PLATFORM_WINDOWS=1 -EHsc -Zi -I ..\Headers -I ..\Windows\Headers ..\keypad.cpp ..\Windows\*.cpp ..\Code\*.cpp User32.lib XInput.lib Advapi32.lib
+cl -D PLATFORM_WINDOWS=1 -EHsc -Zi -I ..\Headers -I ..\Windows\Headers ..\keypadservice.cpp ..\Windows\*.cpp ..\Code\*.cpp User32.lib XInput.lib Advapi32.lib
 
 popd
