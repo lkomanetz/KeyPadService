1edc21b66ccc8e5b00bf1e66b2ba35e131ef96cb
Logan <lkomanetz@gmail.com>
2022-04-14 22:29:38 -0500

    Adds ability to include an input parameter, treat lines that start with # symbol as comments, and an example mapping file.

---
+++ Headers/inputparameterparser.h
@@ -0,0 +1,17 @@
+#ifndef INPUTPARAMETERPARSER_H
+#define INPUTPARAMETERPARSER_H
+
+using namespace std;
+
+class InputParameterParser
+{
+private:
+    vector<string> _tokens;
+
+public:
+    InputParser(int& argc, char** argv);
+    bool cmdOptionExists(const string& option) const;
+    const string& getCmdOption(const string& option) const;
+}
+
+#endif
